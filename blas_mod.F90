a33113aa4634bd4f028957a45b01c4d53f86e3b9
Ed D'Azevedo <dazevedoef@ornl.gov>
2021-01-21 19:58:37 -0600

    add dnrm2

--- blas_mod.F90
+++ blas_mod.F90
@@ -13,12 +13,27 @@
           real*8 :: x(*),y(*)
           end function ddot
 
-        subroutine dgemv(trans,m,n,A,lda,alpha,x,incx,beta,y,incy)
+          real*8 function dnrm2(n,x,incx)
+          implicit none
+          integer :: n,incx
+          real*8 :: x(*)
+          end function dnrm2
+
+        subroutine dgemv(trans,m,n,alpha,A,lda,x,incx,beta,y,incy)
         implicit none
         character :: trans
         integer :: m,n,lda,incx,incy
         real*8 :: alpha, beta, A(lda,*),x(*),y(*)
         end subroutine dgemv
+
+        subroutine dgemm(transA,transB,m,n,k,                            &
+     &   alpha,A,lda,B,ldb,beta,C,ldc )
+        implicit none
+        character transA,transB
+        integer m,n,k,lda,ldb,ldc
+        real*8 alpha,beta
+        real*8 A(lda,*),B(ldb,*),C(ldc,*)
+        end subroutine dgemm
 
         end interface
 
